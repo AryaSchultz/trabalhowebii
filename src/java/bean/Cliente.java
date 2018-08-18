b479503f3822ffa67f0aa40e20c1e5d83eba2bcf
sa <sa@sa-Aspire-E5-571>
2018-08-18 14:03:47 -0300

    Criacao de DAOs e classes

---
+++ src/java/bean/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author sa
+ */
+public class Cliente implements Serializable{
+    private int id;
+    private String nome;
+    private String senha;
+    private String email;
+    private String CPF;    
+    private String atendimentos;
+
+}
