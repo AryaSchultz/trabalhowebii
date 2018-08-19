74c4627ecea3b1229e53abf9add76a31ecc7c59e
sa <sa@sa-Aspire-E5-571>
2018-08-19 10:28:08 -0300

    Criacao de cidade, estado, getter e setter

--- src/java/bean/Cliente.java
+++ src/java/bean/Cliente.java
@@ -19,4 +19,63 @@ public class Cliente implements Serializable{
     private String CPF;    
     private String atendimentos;
 
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String senha, String email, String CPF, String atendimentos) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.CPF = CPF;
+        this.atendimentos = atendimentos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getAtendimentos() {
+        return atendimentos;
+    }
+
+    public void setAtendimentos(String atendimentos) {
+        this.atendimentos = atendimentos;
+    }
+
 }
